c69260099c9d2a798890cb4b6b5111a2e3a02594
ImDef4ult <a.rojasb@uniandes.edu.co>
2021-12-04 18:49:52 -0500

    Initial commit

---
+++ DL.py
@@ -0,0 +1,194 @@
+import streamlit as st
+from PIL import Image
+from utils import (
+    draw_spectrogram,
+    extract_features,
+    predict
+)
+
+
+def main():
+    # --------------------------------
+    # Side Bar
+    # --------------------------------
+    Side = st.sidebar
+    Side.header('Try it out!!')
+    user_file = Side.file_uploader('Upload your audio file')
+
+    # Banner Image
+    imageHome = Image.open('Resources/Banner.jpg')
+    st.image(imageHome, use_column_width=True)
+    st.title('Audio Recognition')
+
+    if not user_file:
+        About = st.expander("Acerca de")
+        About.markdown("""
+            * **Autores:** Daniel Andres Jimenez, Alejandro Leon Andrade, Andres Felipe Rojas
+            * **Materia:** Deep Learning
+            * **Librerias utilizadas:** pandas, streamlit, PIL, Keras, librosa
+            * **Fuente de datos:** [UrbanSounds8k](https://urbansounddataset.weebly.com/urbansound8k.html).
+            """)
+        st.header('Planteamiento del problema')
+        st.write('''
+            Dados los bajos costes en sensores, y teniendo en cuenta la vida en grandes ciudades como Bogotá, 
+            es interesante identificar los diferentes sonidos urbanos presentes en toda la ciudad. 
+            Al tener identificadas diferentes zonas de la ciudad con cierto tipo de sonidos, instituciones como 
+            La secretaria Distrital de Ambiente, pueden tomar decisiones concretas y rápidas, respecto a la 
+            contaminación ambiental presentada. En futuros trabajos es posible combinar la identificación del 
+            sonido junto con los niveles de intensidad del sonido, para tomar mejores decisiones.
+        ''')
+
+        st.header('Objetivo de negocio')
+        st.write('''
+            Realizar un modelo de aprendizaje profundo que sea capaz de identificar y clasificar sonidos urbanos en 10 
+            categorías iniciales y básicas de la taxonomía de sonidos urbanos.
+        ''')
+
+        st.header('Como son los datos?')
+        air, car, kids, dog = st.columns(4)
+        with air:
+            # Air conditioner
+            st.subheader('Aire acondicionado')
+
+            air_image = Image.open('Resources/air_conditioner.jpg')
+            air.image(air_image, use_column_width=True)
+
+            file_path = 'samples/air.wav'
+            air.pyplot(draw_spectrogram(file_path))
+            st.audio(file_path)
+        with car:
+            # Car horn
+            st.subheader('Bocina de auto')
+
+            car_image = Image.open('Resources/car_horn.jpg')
+            car.image(car_image, use_column_width=True)
+
+            file_path = 'samples/car.wav'
+            car.pyplot(draw_spectrogram(file_path))
+            st.audio(file_path)
+
+        with kids:
+            # Children playing
+            st.subheader('Niños jugando')
+            kid_image = Image.open('Resources/children_playing.jpg')
+            kids.image(kid_image, use_column_width=True)
+
+            file_path = 'samples/children.wav'
+            kids.pyplot(draw_spectrogram(file_path))
+            st.audio(file_path)
+
+        with dog:
+            # Dog bark
+            st.subheader('Perro ladrando')
+
+            dog_image = Image.open('Resources/dog_bark.jpg')
+            dog.image(dog_image, use_column_width=True)
+
+            file_path = 'samples/dog.wav'
+            dog.pyplot(draw_spectrogram(file_path))
+            st.audio(file_path)
+
+        drill, eng, gun, ham = st.columns(4)
+        with drill:
+            # Drilling
+            st.subheader('Taladro')
+
+            car_image = Image.open('Resources/drilling.jpg')
+            drill.image(car_image, use_column_width=True)
+
+            file_path = 'samples/drilling.wav'
+            drill.pyplot(draw_spectrogram(file_path))
+            st.audio(file_path)
+
+        with eng:
+            # Engine Idling
+            st.subheader('Encendiendo motor')
+            engine_image = Image.open('Resources/engine_idling.jpg')
+            eng.image(engine_image, use_column_width=True)
+
+            file_path = 'samples/engine.wav'
+            eng.pyplot(draw_spectrogram(file_path))
+            st.audio(file_path)
+
+        with gun:
+            # Gun shot
+            st.subheader('Disparo de arma')
+
+            gun_image = Image.open('Resources/gun_shot.jpg')
+            gun.image(gun_image, use_column_width=True)
+
+            file_path = 'samples/gun.wav'
+            gun.pyplot(draw_spectrogram(file_path))
+            st.audio(file_path)
+
+        with ham:
+            # Jackhammer
+            st.subheader('Martillo neumático')
+
+            hammer_image = Image.open('Resources/jackhammer.jpg')
+            ham.image(hammer_image, use_column_width=True)
+
+            file_path = 'samples/hammer.wav'
+            ham.pyplot(draw_spectrogram(file_path))
+            st.audio(file_path)
+
+        siren, street = st.columns(2)
+        with siren:
+            # Siren
+            st.subheader('Sirena')
+            siren_image = Image.open('Resources/siren.jpg')
+            siren.image(siren_image, use_column_width=True)
+
+            file_path = 'samples/siren.wav'
+            siren.pyplot(draw_spectrogram(file_path))
+            st.audio(file_path)
+
+        with street:
+            # Street music
+            st.subheader('Música callejera')
+            music_image = Image.open('Resources/street_music.jpg')
+            street.image(music_image, use_column_width=True)
+
+            file_path = 'samples/street.wav'
+            street.pyplot(draw_spectrogram(file_path))
+            st.audio(file_path)
+
+        st.header('Modelado')
+        mlp, cnn = st.columns(2)
+        with mlp:
+            mlp.subheader('MLP')
+        with cnn:
+            cnn.subheader('CNN')
+
+    else:
+        st.header(user_file.name)
+        st.audio(user_file)
+
+        st.subheader('Extracción de características')
+        # st.write(extract_features(user_file))
+
+        st.subheader('Predicción')
+        clase, proba = predict(user_file, 'cnn')
+        details, img = st.columns((1, 2))
+        with details:
+            details.subheader(f'Clase: {clase}')
+            details.dataframe(proba.sort_values(by=['Prob'], ascending=False))
+
+        with img:
+            pred_img = Image.open(f'Resources/{clase}_2.jpg')
+            img.image(pred_img, use_column_width=True)
+
+
+
+if __name__ == '__main__':
+    st.set_page_config(
+        page_title='Audio Recognition',
+        page_icon=':music:',
+        layout='wide',
+        initial_sidebar_state='collapsed',
+        menu_items={
+            'Get Help': 'https://www.extremelycoolapp.com/help',
+            'Report a bug': "https://www.extremelycoolapp.com/bug"
+        }
+    )
+    main()
---
+++ utils.py
@@ -0,0 +1,88 @@
+import matplotlib.pyplot as plt
+import librosa
+import io
+import keras
+import numpy as np
+from keras.models import model_from_json
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+from tensorflow.keras.utils import to_categorical
+
+max_pad_len = 174
+num_rows = 40
+num_columns = 174
+num_channels = 1
+
+
+def draw_spectrogram(file_name):
+    librosa_audio, librosa_rate = librosa.load(file_name)
+    plt.figure(figsize=(12, 4))
+    plt.plot(librosa_audio)
+    return plt
+
+
+def load_model(model):
+    try:
+        # Load the model
+        json_file = open(f'models/cnn.json')
+        json_model = json_file.read()
+        json_file.close()
+
+        # Load the weights
+        model = model_from_json(json_model)
+        model.load_weights(f'models/cnn.h5')
+        print(f'Model {model} loaded! ')
+        return model
+    except Exception as e:
+        print(f'Error cargando el modelo {model}: {e}')
+        return None
+
+
+def extract_features(file_name):
+    try:
+        audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
+        mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+        pad_width = max_pad_len - mfccs.shape[1]
+        mfccs = np.pad(mfccs, pad_width=((0, 0), (0, pad_width)), mode='constant')
+
+    except Exception as e:
+        print("Error encountered while parsing file: ", file_name)
+        return None
+    return mfccs
+
+
+def predict(file, type):
+    probabilities = pd.DataFrame(columns=['Category', 'Prob'])
+    model = cnn if type is 'cnn' else mlp
+    prediction_feature = extract_features(file)
+    prediction_feature = prediction_feature.reshape(1, num_rows, num_columns, num_channels)
+
+    predicted_vector = model.predict(prediction_feature)
+    classes_x = np.argmax(predicted_vector, axis=1)
+    predicted_class = le.inverse_transform(classes_x)
+    print("The predicted class is:", predicted_class[0], '\n')
+
+    predicted_proba_vector = model.predict(prediction_feature)
+    predicted_proba = predicted_proba_vector[0]
+    for i in range(len(predicted_proba)):
+        category = le.inverse_transform(np.array([i]))
+        probabilities.loc[len(probabilities)] = [category[0], format(predicted_proba[i], '.32f')]
+        # print(category[0], "\t\t : ", format(predicted_proba[i], '.32f'))
+
+    return predicted_class[0], probabilities
+
+try:
+    # Load CNN model
+    cnn = load_model('cnn')
+
+    # Load MLP model
+    mlp = load_model('mlp')
+
+    # Load features
+    features_df = pd.read_csv('Resources/features_df.csv')
+    X = np.array(features_df.feature.tolist())
+    y = np.array(features_df.class_label.tolist())
+    le = LabelEncoder()
+    yy = to_categorical(le.fit_transform(y))
+except Exception as e:
+    print(f'Error loading the models: {e}')
